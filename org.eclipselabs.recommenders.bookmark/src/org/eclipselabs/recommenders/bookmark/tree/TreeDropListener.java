acd68843b771f78346fa4378c392cb64f596665f
Toobee <Toobee@Toobees-MacBook-Pro.local>
2011-11-09 20:48:21 +0100

    Kommentar gelöscht

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/tree/TreeDropListener.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/tree/TreeDropListener.java
@@ -67,7 +67,6 @@ public class TreeDropListener implements DropTargetListener {
 				return false;
 			}
 
-			// Is multi-selection one of the selected nodes
 			if (refNode == target)
 				return false;
 
