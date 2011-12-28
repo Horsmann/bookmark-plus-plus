b38eaeb692ab97511d0ffe1aae3dc63da6d72154
Tobias Horsmann <tobias.horsmann@gmail.com>
2011-12-28 14:11:43 +0100

    Ungenutzes Interface aus Klasse entfernt

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/actions/CloseAllOpenEditorsAction.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/actions/CloseAllOpenEditorsAction.java
@@ -1,13 +1,10 @@
 package org.eclipselabs.recommenders.bookmark.view.actions;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 import org.eclipselabs.recommenders.bookmark.Activator;
 import org.eclipselabs.recommenders.bookmark.tree.commands.CloseAllOpenEditors;
 
-public class CloseAllOpenEditorsAction extends Action implements SelfEnabling{
+public class CloseAllOpenEditorsAction extends Action {
 
 	public CloseAllOpenEditorsAction() {
 		this.setImageDescriptor(Activator.getDefault().getImageRegistry()
@@ -19,12 +16,6 @@ public class CloseAllOpenEditorsAction extends Action implements SelfEnabling{
 	@Override
 	public void run() {
 		new CloseAllOpenEditors().execute();
-	}
-
-	@Override
-	public void updateEnabledStatus() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
