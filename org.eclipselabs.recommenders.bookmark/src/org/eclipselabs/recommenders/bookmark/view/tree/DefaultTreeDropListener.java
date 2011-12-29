b0d786a66b788f6925e5b49104408d30af7bea37
Tobias Horsmann <tobias.horsmann@gmail.com>
2011-12-29 17:15:46 +0100

    Knotensortieren generalisiert, funktioniert jetzt für alle Knoten

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/tree/DefaultTreeDropListener.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/tree/DefaultTreeDropListener.java
@@ -16,6 +16,7 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipselabs.recommenders.bookmark.tree.BMNode;
 import org.eclipselabs.recommenders.bookmark.tree.TreeModel;
+import org.eclipselabs.recommenders.bookmark.tree.TreeNode;
 import org.eclipselabs.recommenders.bookmark.tree.commands.AddTreeNodesToExistingBookmark;
 import org.eclipselabs.recommenders.bookmark.tree.commands.AddTreeNodesToNewBookmark;
 import org.eclipselabs.recommenders.bookmark.tree.commands.AddTreepathsToExistingBookmark;
@@ -41,7 +42,7 @@ public class DefaultTreeDropListener
 	@Override
 	public void drop(DropTargetEvent event)
 	{
-		
+
 		manager.reinitializeExpandedStorage();
 		manager.addCurrentlyExpandedNodesToStorage();
 
@@ -127,8 +128,9 @@ public class DefaultTreeDropListener
 	private void processDropEventWithDragFromWithinTheView(DropTargetEvent event)
 		throws JavaModelException
 	{
-		if (test(event))
+		if (performReorderNodeOperation(event)) {
 			return;
+		}
 
 		BMNode bookmark = getBookmarkOfDropTarget(event);
 
@@ -141,90 +143,124 @@ public class DefaultTreeDropListener
 
 	}
 
-	private boolean test(DropTargetEvent event)
+	private boolean performReorderNodeOperation(DropTargetEvent event)
 	{
 		BookmarkView activeBookmarkView = manager.getActiveBookmarkView();
 		List<IStructuredSelection> treeSelections = TreeUtil
 				.getTreeSelections(activeBookmarkView.getView());
 
-		LinkedList<BMNode> modelCategories = getModelCategories();
-
-		LinkedList<BMNode> selectedNodes = new LinkedList<BMNode>();
 		BMNode target = (BMNode) getTarget(event);
+
+		LinkedList<BMNode> silblings = getSilblings(target);
+
+		LinkedList<BMNode> selectedSilblings = new LinkedList<BMNode>();
+		LinkedList<BMNode> unselectedSilblings = new LinkedList<BMNode>(
+				silblings);
 
 		for (int i = 0; i < treeSelections.size(); i++) {
 			BMNode node = (BMNode) treeSelections.get(i);
-			if (invalidNodeForOperation(node, target)) {
+			if (isInvalidNodeForReorderOperation(node, target)) {
 				return false;
 			}
 
-			selectedNodes.add(node);
-			// remove a selected category since this node is going to be moved
-			modelCategories.remove(node);
+			selectedSilblings.add(node);
+			unselectedSilblings.remove(node);
 		}
 
 		WidgetElementLocation wel = new WidgetElementLocation(event,
 				activeBookmarkView);
 
-		reorderCategoryNodesInModel(wel, modelCategories, selectedNodes);
+		reorderNodes(wel, unselectedSilblings, selectedSilblings);
 
 		return true;
 	}
 
-	private void reorderCategoryNodesInModel(WidgetElementLocation wel,
-			LinkedList<BMNode> modelCategories, LinkedList<BMNode> selectedNodes)
+	private void reorderNodes(WidgetElementLocation wel,
+			LinkedList<BMNode> unselectedSilblings,
+			LinkedList<BMNode> selectedSilblings)
 	{
 		BMNode target = wel.element;
-		LinkedList<BMNode> newCategoryOrder = new LinkedList<BMNode>();
+		LinkedList<BMNode> newOrder = new LinkedList<BMNode>();
 
-		int modelIndex=0;
-		for (BMNode node : modelCategories) {
+		int index = moveUnselectedNodesToNewOrderUntilTargetNode(
+				unselectedSilblings, newOrder, target);
+
+		if (wel.isInUpperHalf) {
+			processDropInUpperHalf(selectedSilblings, unselectedSilblings,
+					newOrder, index);
+		}
+		else {
+			processDropInLowerHalf(selectedSilblings, unselectedSilblings,
+					newOrder, index);
+		}
+
+		setNewOrder(target, newOrder);
+
+		manager.getActiveBookmarkView().updateControls();
+		manager.setStoredExpandedNodesForActiveView();
+	}
+
+	private void setNewOrder(BMNode target, LinkedList<BMNode> newOrder)
+	{
+		BMNode parent = target.getParent();
+		parent.removeAllChildren();
+
+		for (BMNode node : newOrder) {
+			parent.addChild(node);
+		}
+
+	}
+
+	private void processDropInLowerHalf(LinkedList<BMNode> selectedSilblings,
+			LinkedList<BMNode> unselectedSilblings,
+			LinkedList<BMNode> newOrder, int modelIndex)
+	{
+		newOrder.add(unselectedSilblings.get(modelIndex));
+
+		for (BMNode node : selectedSilblings) {
+			newOrder.add(node);
+		}
+
+		for (int i = (modelIndex + 1); i < unselectedSilblings.size(); i++) {
+			newOrder.add(unselectedSilblings.get(i));
+		}
+
+	}
+
+	private void processDropInUpperHalf(LinkedList<BMNode> selectedSilblings,
+			LinkedList<BMNode> unselectedSilblings,
+			LinkedList<BMNode> newOrder, int modelIndex)
+	{
+		for (BMNode node : selectedSilblings) {
+			newOrder.add(node);
+		}
+		for (int i = modelIndex; i < unselectedSilblings.size(); i++) {
+			newOrder.add(unselectedSilblings.get(i));
+		}
+	}
+
+	private int moveUnselectedNodesToNewOrderUntilTargetNode(
+			LinkedList<BMNode> unselectedSilblings,
+			LinkedList<BMNode> newOrder, BMNode target)
+	{
+		int modelIndex = 0;
+		for (BMNode node : unselectedSilblings) {
 			if (node != target) {
-				newCategoryOrder.add(node);
+				newOrder.add(node);
 			}
 			else {
 				break;
 			}
 			modelIndex++;
 		}
-		
-		if (wel.isInUpperHalf) {
-			for (BMNode node : selectedNodes) {
-				newCategoryOrder.add(node);
-			}
-			for (int i=modelIndex; i < modelCategories.size(); i++){
-				newCategoryOrder.add(modelCategories.get(i));
-			}
-		}
-		else { 
-			
-			newCategoryOrder.add(modelCategories.get(modelIndex));
-			
-			for (BMNode node : selectedNodes) {
-				newCategoryOrder.add(node);
-			}
-			
-			for (int i=(modelIndex+1); i < modelCategories.size(); i++){
-				newCategoryOrder.add(modelCategories.get(i));
-			}
-			
-		}
 
-		BMNode root = manager.getModel().getModelRoot();
-		root.removeAllChildren();
-		
-		for (BMNode node: newCategoryOrder) {
-			root.addChild(node);
-		}
-		
-		manager.getActiveBookmarkView().updateControls();
-		manager.setStoredExpandedNodesForActiveView();
+		return modelIndex;
 	}
 
-	private boolean invalidNodeForOperation(BMNode node, BMNode target)
+	private boolean isInvalidNodeForReorderOperation(BMNode node, BMNode target)
 	{
-		if (!node.isBookmarkNode()) {
-			return true;// DUMMY return
+		if (node.getParent() != target.getParent()) {
+			return true;
 		}
 		if (node == target) {
 			return true;
@@ -232,16 +268,15 @@ public class DefaultTreeDropListener
 		return false;
 	}
 
-	private LinkedList<BMNode> getModelCategories()
+	private LinkedList<BMNode> getSilblings(BMNode target)
 	{
-		LinkedList<BMNode> modelCategories = new LinkedList<BMNode>();
+		LinkedList<BMNode> silblings = new LinkedList<BMNode>();
 
-		BMNode[] modelCategoryNodes = manager.getModel().getModelRoot()
-				.getChildren();
+		BMNode[] modelCategoryNodes = target.getParent().getChildren();
 		for (BMNode node : modelCategoryNodes) {
-			modelCategories.add(node);
+			silblings.add(node);
 		}
-		return modelCategories;
+		return silblings;
 	}
 
 	private void addToNewBookmark(DropTargetEvent event)
@@ -340,26 +375,26 @@ public class DefaultTreeDropListener
 		public WidgetElementLocation(DropTargetEvent event,
 				BookmarkView bookmarView)
 		{
-			Point p = new Point(event.x, event.y);
-
 			element = (BMNode) getTarget(event);
 
 			TreeViewer viewer = bookmarView.getView();
 
-			Point control = viewer.getTree().toControl(p);
-
-			TreeItem item = viewer.getTree().getItem(control);
-
+			// get the rectangle are of the target control
+			Point globalPoint = new Point(event.x, event.y);
+			Point controlRelativePoint = viewer.getTree()
+					.toControl(globalPoint);
+			TreeItem item = viewer.getTree().getItem(controlRelativePoint);
 			Rectangle bounds = item.getBounds();
 
+			// divide target rectangle in upper an lower half
 			Rectangle upperHalf = new Rectangle(bounds.x, bounds.y,
 					bounds.width, bounds.height / 2);
-
 			Rectangle lowerHalf = new Rectangle(bounds.x, bounds.y
 					+ (bounds.height / 2), bounds.width, bounds.height / 2);
 
-			isInUpperHalf = upperHalf.contains(control);
-			isInLowerHalf = lowerHalf.contains(control);
+			// decide in which part drop occurred
+			isInUpperHalf = upperHalf.contains(controlRelativePoint);
+			isInLowerHalf = lowerHalf.contains(controlRelativePoint);
 		}
 
 	}
