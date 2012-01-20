54e97dbc3eb96475b8835f7e4081be355175d0cd
Tobias Horsmann <tobias.horsmann@gmail.com>
2012-01-20 18:18:42 +0100

    Bookmarks von geschlossenen Projekten werden nicht mehr geöffnet

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/aaa/visitor/OpenInEditorVisitor.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/aaa/visitor/OpenInEditorVisitor.java
@@ -1,5 +1,12 @@
 package org.eclipselabs.recommenders.bookmark.aaa.visitor;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IEditorPart;
@@ -24,7 +31,14 @@ public class OpenInEditorVisitor implements IModelVisitor {
     @Override
     public void visit(FileBookmark fileBookmark) {
         try {
-            IDE.openEditor(activePage, fileBookmark.getFile());
+
+            IsResourceAvailableVisitor availVisitor = new IsResourceAvailableVisitor();
+            fileBookmark.accept(availVisitor);
+
+            if (availVisitor.isAvailable()) {
+                IDE.openEditor(activePage, fileBookmark.getFile());
+            }
+
         } catch (PartInitException e) {
             e.printStackTrace();
         }
@@ -40,8 +54,14 @@ public class OpenInEditorVisitor implements IModelVisitor {
     @Override
     public void visit(JavaElementBookmark javaElementBookmark) {
         try {
-            IEditorPart part = JavaUI.openInEditor(javaElementBookmark.getJavaElement());
-            JavaUI.revealInEditor(part, javaElementBookmark.getJavaElement());
+
+            IsResourceAvailableVisitor availVisitor = new IsResourceAvailableVisitor();
+            javaElementBookmark.accept(availVisitor);
+
+            if (availVisitor.isAvailable()) {
+                IEditorPart part = JavaUI.openInEditor(javaElementBookmark.getJavaElement());
+                JavaUI.revealInEditor(part, javaElementBookmark.getJavaElement());
+            }
         } catch (PartInitException e) {
             e.printStackTrace();
         } catch (JavaModelException e) {
@@ -53,6 +73,64 @@ public class OpenInEditorVisitor implements IModelVisitor {
                 child.accept(this);
             }
         }
+    }
+
+    private class IsResourceAvailableVisitor implements IModelVisitor {
+
+        private boolean isAvailable = false;
+
+        public boolean isAvailable() {
+            return isAvailable;
+        }
+
+        @Override
+        public void visit(FileBookmark fileBookmark) {
+            IFile file = fileBookmark.getFile();
+            IProject project = file.getProject();
+            boolean isOpen = project.isOpen();
+            boolean exists = file.exists();
+
+            isAvailable = isOpen && exists;
+        }
+
+        @Override
+        public void visit(Category category) {
+            isAvailable = true;
+        }
+
+        @Override
+        public void visit(JavaElementBookmark javaElementBookmark) {
+            IJavaElement element = javaElementBookmark.getJavaElement();
+            IProject project = element.getJavaProject().getProject();
+            boolean isOpen = project.isOpen();
+            boolean exists = doesReferencedIJavaElementExist(element);
+
+            isAvailable = isOpen && exists;
+        }
+
+        private boolean doesReferencedIJavaElementExist(IJavaElement element) {
+            if (element instanceof IField) {
+                IField field = (IField) element;
+                return field.exists();
+            }
+
+            if (element instanceof IMethod) {
+                IMethod method = (IMethod) element;
+                return method.exists();
+            }
+
+            if (element instanceof IType) {
+                IType type = (IType) element;
+                return type.exists();
+            }
+
+            if (element instanceof ICompilationUnit) {
+                IJavaElement unit = (ICompilationUnit) element;
+                return unit.exists();
+            }
+            return false;
+        }
+
     }
 
 }
