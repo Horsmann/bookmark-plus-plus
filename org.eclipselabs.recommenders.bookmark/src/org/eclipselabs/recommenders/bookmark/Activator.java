0a6b66c1f686e018c8dbe084c5f85a0d2a03a838
Tobias Horsmann <tobias.horsmann@gmail.com>
2012-01-29 21:41:51 +0100

    tote Icons entfernt

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/Activator.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/Activator.java
@@ -29,20 +29,15 @@ public class Activator extends AbstractUIPlugin {
     private static BookmarkView bookmarkView;
 
     public static final String ICON_DEFAULT = "default";
-    public static final String ICON_CATEGORY = "bookmark";
+    public static final String ICON_CATEGORY = "category";
     public static final String ICON_SHOW_IN_EDITOR = "openBookmarksInEditor";
     public static final String ICON_CLOSE_ALL_OPEN_EDITORS = "closeAllOpenEditors";
-    public static final String ICON_SAVE_BOOKMARKS = "exportBookmarks";
-    public static final String ICON_LOAD_BOOKMARKS = "importBookmarks";
-    public static final String ICON_ASSOCIATED_PROJECT_CLOSED = "projectClosed";
-    public static final String ICON_ASSOCIATED_RESOURCE_NOT_AVAILABLE = "projectNotAvailable";
-    public static final String ICON_REFRESH_VIEW = "refreshView";
     public static final String ICON_OPEN_IN_SYSTEM_EXPLORER = "openInSystemExplorer";
     public static final String ICON_TOGGLE_VIEW = "toggleLevel";
     public static final String ICON_DELETE = "delete";
     public static final String ICON_RENAME_CATEGORY = "rename";
     public static final String ICON_TOGGLE_FLAT_HIERARCHY = "toggleBetweenFlatAndTreeModus";
-    public static final String ICON_SAVE = "saveChanges";
+   
 
     public static final String AUTOSAVE_FILE = "bookmarkModel" + fileEnding;
 
@@ -97,23 +92,8 @@ public class Activator extends AbstractUIPlugin {
         imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/read_obj.gif"), null));
         registry.put(ICON_SHOW_IN_EDITOR, imgDesc);
 
-        imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/export_wiz.gif"), null));
-        registry.put(ICON_SAVE_BOOKMARKS, imgDesc);
-
-        imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/import_wiz.gif"), null));
-        registry.put(ICON_LOAD_BOOKMARKS, imgDesc);
-
         imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/clear_co.gif"), null));
         registry.put(ICON_CLOSE_ALL_OPEN_EDITORS, imgDesc);
-
-//        imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/deadlock_view.gif"), null));
-//        registry.put(ICON_ASSOCIATED_PROJECT_CLOSED, imgDesc);
-//
-//        imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/errorwarning_tab.gif"), null));
-//        registry.put(ICON_ASSOCIATED_RESOURCE_NOT_AVAILABLE, imgDesc);
-//
-//        imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/refresh_tab.gif"), null));
-//        registry.put(ICON_REFRESH_VIEW, imgDesc);
 
         imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/opentype.gif"), null));
         registry.put(ICON_OPEN_IN_SYSTEM_EXPLORER, imgDesc);
@@ -129,9 +109,6 @@ public class Activator extends AbstractUIPlugin {
 
         imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/tree_explorer.gif"), null));
         registry.put(ICON_TOGGLE_FLAT_HIERARCHY, imgDesc);
-
-//        imgDesc = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path("icons/save_edit.gif"), null));
-//        registry.put(ICON_SAVE, imgDesc);
 
     }
 
