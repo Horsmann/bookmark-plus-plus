20309816ea0702a8695ccf74e39e2d4d594587ef
Tobias Horsmann <tobias.horsmann@gmail.com>
2012-01-30 14:05:09 +0100

    Umgebung wieder lauffähig

---
+++ org.eclipselabs.recommenders.bookmark.tests/src/org/eclipselabs/recommenders/bookmark/DummyTest.java
@@ -0,0 +1,14 @@
+package org.eclipselabs.recommenders.bookmark;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
