0d86cbfb273597601cfe1b7e2b8537519f052a7f
Tobias Horsmann <tobias.horsmann@gmail.com>
2012-02-20 17:21:45 +0100

    Bookmarken von TreeSelections über Short-Cut

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/handler/BookmarkCurrentPositionHandler.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/handler/BookmarkCurrentPositionHandler.java
@@ -1,16 +1,34 @@
 package org.eclipselabs.recommenders.bookmark.view.handler;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.ITypeRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
+import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.swt.internal.cocoa.objc_super;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipselabs.recommenders.bookmark.Activator;
+import org.eclipselabs.recommenders.bookmark.commands.AddElementToModelCommand;
+import org.eclipselabs.recommenders.bookmark.model.IBookmarkModelComponent;
 import org.eclipselabs.recommenders.bookmark.view.BookmarkCommandInvoker;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 
 public class BookmarkCurrentPositionHandler extends AbstractHandler {
 
@@ -28,24 +46,55 @@ public class BookmarkCurrentPositionHandler extends AbstractHandler {
         ISelection selection = activePage.getSelection();
 
         if (selection instanceof ITreeSelection) {
-            Object firstElement = ((ITreeSelection) selection).getFirstElement();
-            IJavaElement javaElement = null;
-            if (firstElement instanceof IJavaElement) {
-                javaElement = (IJavaElement) firstElement;
-                System.out.println(javaElement.getHandleIdentifier());
-            }
-        } //else if (part instanceof JavaEditor && selection instanceof ITextSelection) {
-//            final IEditorInput input = ((JavaEditor)part).getEditorInput();
-//            final ITypeRoot root = (ITypeRoot) JavaUI.getEditorInputJavaElement(input);
-//            final IJavaElement[] elements = root.codeSelect(selection.getOffset(), 0);
-//            if (elements.length > 0) {
-//                javaElement = elements[0];
-//                System.out.println(javaElement.getHandleIdentifier());
-//            }
-//        }
+            processTreeSelection((ITreeSelection) selection);
+        }
 
-        System.err.println("Executing BookmarkCurrentPositionHandler");
+        // if (part instanceof IEditorPart && selection instanceof
+        // ITextSelection) {
+        // processEditorSelection((IEditorPart)part, (ITextSelection)selection);
+        //
+        // int a = 0;
+        // a++;
+        // }
+
         return null;
     }
 
+    private void processEditorSelection(IEditorPart part, ITextSelection selection) {
+        IEditorInput editorInput = ((IEditorPart) part).getEditorInput();
+        final ITypeRoot root = (ITypeRoot) JavaUI.getEditorInputJavaElement(editorInput);
+        IJavaElement[] elements = null;
+        try {
+            int offset = ((ITextSelection) selection).getOffset();
+            elements = root.codeSelect(offset, 0);
+        } catch (JavaModelException e) {
+            e.printStackTrace();
+        }
+        if (elements != null && elements.length > 0) {
+            IJavaElement javaElement = elements[0];
+            System.out.println(javaElement.getHandleIdentifier());
+        }
+        String handleIdentifier = root.getHandleIdentifier();
+        IJavaElement create = JavaCore.create(handleIdentifier);
+    }
+
+    private void processTreeSelection(ITreeSelection selection) {
+
+        List<Object> objects = Lists.newArrayList();
+        @SuppressWarnings("rawtypes")
+        Iterator iterator = selection.iterator();
+
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+            if (next instanceof IJavaElement) {
+                objects.add(next);
+            } else if (next instanceof IFile) {
+                objects.add(next);
+            }
+        }
+
+        Optional<IBookmarkModelComponent> dropTarget = Optional.absent();
+        Optional<String> nameForNewCategory = Optional.absent();
+        invoker.invoke(new AddElementToModelCommand(objects.toArray(), invoker, false, dropTarget, nameForNewCategory));
+    }
 }
