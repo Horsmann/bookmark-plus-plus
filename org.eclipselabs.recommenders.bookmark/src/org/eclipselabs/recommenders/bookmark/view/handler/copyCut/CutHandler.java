f7a9d801b33e11f6423e5e03893ad4c6e457146e
Tobias Horsmann <tobias.horsmann@gmail.com>
2012-02-27 21:25:27 +0100

    Cut-Handler für das rekursive Löschen angepasst

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/handler/copyCut/CutHandler.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/view/handler/copyCut/CutHandler.java
@@ -3,9 +3,12 @@ package org.eclipselabs.recommenders.bookmark.view.handler.copyCut;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipselabs.recommenders.bookmark.commands.BookmarkDeletionCommand;
+import org.eclipselabs.recommenders.bookmark.commands.DeleteInferredBookmarksCommand;
+import org.eclipselabs.recommenders.bookmark.model.IBookmark;
 import org.eclipselabs.recommenders.bookmark.model.IBookmarkModelComponent;
 import org.eclipselabs.recommenders.bookmark.view.BookmarkCommandInvoker;
 import org.eclipselabs.recommenders.bookmark.view.tree.RepresentationSwitchableTreeViewer;
+import org.eclipselabs.recommenders.bookmark.visitor.IsCategoryVisitor;
 
 public class CutHandler extends CopyHandler {
 
@@ -22,10 +25,22 @@ public class CutHandler extends CopyHandler {
         super.execute(event);
 
         for (IBookmarkModelComponent component : selectedComponents) {
+            IBookmarkModelComponent parent = component.getParent();
+            
             invoker.invoke(new BookmarkDeletionCommand(component));
+            if (needsCheckForRecursiveDeletion(parent)) {
+                invoker.invoke(new DeleteInferredBookmarksCommand((IBookmark) parent));
+            }
+            
         }
 
         return null;
+    }
+
+    private boolean needsCheckForRecursiveDeletion(IBookmarkModelComponent parent) {
+        IsCategoryVisitor isCat = new IsCategoryVisitor();
+        parent.accept(isCat);
+        return (isCat.isCategory() == false);
     }
 
 }
