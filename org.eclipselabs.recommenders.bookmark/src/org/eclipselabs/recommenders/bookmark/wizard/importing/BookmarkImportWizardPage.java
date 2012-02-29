05c085301321b2d9bdfedfc2a3fa23cb49ccd030
Tobias Horsmann <tobias.horsmann@gmail.com>
2012-02-29 15:31:15 +0100

    .

--- org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/wizard/importing/BookmarkImportWizardPage.java
+++ org.eclipselabs.recommenders.bookmark/src/org/eclipselabs/recommenders/bookmark/wizard/importing/BookmarkImportWizardPage.java
@@ -379,43 +379,6 @@ public class BookmarkImportWizardPage extends WizardPage implements BookmarkComm
 
     private void addMouseListenerToRemoveButton(Button remove) {
         remove.addMouseListener(new RemoveMouseListener(localTreeViewer, invoker));
-        // remove.addMouseListener(new MouseListener() {
-        //
-        // @Override
-        // public void mouseUp(MouseEvent e) {
-        // }
-        //
-        // @Override
-        // public void mouseDown(MouseEvent e) {
-        // IStructuredSelection selections = localTreeViewer.getSelections();
-        // @SuppressWarnings("rawtypes")
-        // Iterator iterator = selections.iterator();
-        // while (iterator.hasNext()) {
-        // IBookmarkModelComponent component = (IBookmarkModelComponent)
-        // iterator.next();
-        // IBookmarkModelComponent parent = component.getParent();
-        // invoker.invoke(new DeleteSingleBookmarkCommand(component));
-        // deleteInferredNodes(parent);
-        // localTreeViewer.refresh();
-        // }
-        // }
-        //
-        // private void deleteInferredNodes(IBookmarkModelComponent parent) {
-        // if (parent == null) {
-        // return;
-        // }
-        // IsIBookmarkVisitor isBookmark = new IsIBookmarkVisitor();
-        // parent.accept(isBookmark);
-        // if (isBookmark.isIBookmark()) {
-        // invoker.invoke(new DeleteInferredBookmarksCommand((IBookmark)
-        // parent));
-        // }
-        // }
-        //
-        // @Override
-        // public void mouseDoubleClick(MouseEvent e) {
-        // }
-        // });
     }
 
     private void createLoadedFileTreeViewer(Composite bookmarkSelComposite) {
